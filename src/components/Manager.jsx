3ceff59d15c8609c6d4fa96475f83accf89044d8
MalikAasha10 <aashanashraf95@gmail.com>
2025-08-24 09:24:51 +0500

    Added React Toastify

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useRef, useState, useEffect } from 'react'
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Manager = () => {
   const eyeRef = useRef(null);
   const passRef = useRef(null);
@@ -34,12 +36,36 @@ const Manager = () => {
   }
 
   const copyText = (text) => {
+    toast('Copied to clipboard', {
+  position: "top-right",   
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "light",
+  transition: Bounce,      
+});
     navigator.clipboard.writeText(text)
   }
   return (
 
 
     <>
+   <ToastContainer
+  position="top-right"     
+  autoClose={5000}
+  hideProgressBar={false}
+  newestOnTop={false}
+  closeOnClick={false}
+  rtl={false}
+  pauseOnFocusLoss
+  draggable
+  pauseOnHover
+  theme="light"
+  transition={Bounce}      
+/>
       <div className="absolute inset-0 -z-10 h-full w-full
        bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),
        linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
